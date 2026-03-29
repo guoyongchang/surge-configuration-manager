3a8ecb9340f98e5cc03ce662854462891aac2558
guoyongchang <guoyongchang@chanyi.vip>
2026-03-29 22:09:20 +0800

    feat(i18n): simplify i18n.ts to use single locale files

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,44 +2,18 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import enCommon from "./locales/en/common.json";
-import enSubscriptions from "./locales/en/subscriptions.json";
-import enRules from "./locales/en/rules.json";
-import enExtraNodes from "./locales/en/extraNodes.json";
-import enOutput from "./locales/en/output.json";
-import enSettings from "./locales/en/settings.json";
-
-import zhCommon from "./locales/zh/common.json";
-import zhSubscriptions from "./locales/zh/subscriptions.json";
-import zhRules from "./locales/zh/rules.json";
-import zhExtraNodes from "./locales/zh/extraNodes.json";
-import zhOutput from "./locales/zh/output.json";
-import zhSettings from "./locales/zh/settings.json";
+import en from "./locales/en.json";
+import zh from "./locales/zh.json";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: {
-        common: enCommon,
-        subscriptions: enSubscriptions,
-        rules: enRules,
-        extraNodes: enExtraNodes,
-        output: enOutput,
-        settings: enSettings,
-      },
-      zh: {
-        common: zhCommon,
-        subscriptions: zhSubscriptions,
-        rules: zhRules,
-        extraNodes: zhExtraNodes,
-        output: zhOutput,
-        settings: zhSettings,
-      },
+      en: { translation: en },
+      zh: { translation: zh },
     },
     fallbackLng: "en",
-    defaultNS: "common",
     load: "languageOnly",
     detection: {
       order: ["localStorage"],
